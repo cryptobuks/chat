f41db073b9fa0ffd12c4f58be66dce60d87156bc
tzsk <mailtokmahmed@gmail.com>
2018-12-12 21:17:41 +0530

    Updated Security

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBE3IjvbZHqMGgdBN9XBefD-nAVX6H1AtE",
-    authDomain: "vue-chat-tzsk.firebaseapp.com",
-    databaseURL: "https://vue-chat-tzsk.firebaseio.com",
-    projectId: "vue-chat-tzsk",
-    storageBucket: "vue-chat-tzsk.appspot.com",
-    messagingSenderId: "94864749152"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
